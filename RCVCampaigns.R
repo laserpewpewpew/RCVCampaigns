a2c3fd69ca5f4fc1522c66db380ddca6da1caffe
laserpewpewpew <kellen-gracey@uiowa.edu>
2014-05-18 20:04:33 -0500

    final graphs
    last graphs 5/18/2014
    KJG

--- RCVCampaigns.R
+++ RCVCampaigns.R
@@ -106,7 +106,7 @@ delta.myData4 <- qplot(myData4$delta, geom="density") +
 delta.myData4
 
 library(doBy)
-lambdaavgs.myData4 <- summaryBy(lambda ~ d, data=myData3, FUN=c(mean), na.rm=T)
+lambdaavgs.myData4 <- summaryBy(lambda ~ d, data=myData4, FUN=c(mean), na.rm=T)
 
 ########################
 # 5 Candidate Election #
@@ -142,7 +142,7 @@ delta.myData5 <- qplot(myData5$delta, geom="density") +
 delta.myData5
 
 library(doBy)
-lambdaavgs.myData5 <- summaryBy(lambda ~ d, data=myData4, FUN=c(mean), na.rm=T)
+lambdaavgs.myData5 <- summaryBy(lambda ~ d, data=myData5, FUN=c(mean), na.rm=T)
 
 ########################
 # 6 Candidate Election #
@@ -181,125 +181,293 @@ delta.myData6
 library(doBy)
 lambdaavgs.myData6 <- summaryBy(lambda ~ d, data=myData6, FUN=c(mean), na.rm=T)
 
-########################
-# 7 Candidate Election #
-########################
-
-library(foreign)
-myData7 <- read.csv("simdata7cands.csv")
-myData7$lambda <- ((myData7$b*(myData7$y+myData7$d)/(1+myData7$z))-(myData7$c*myData7$k))
-myData7$delta <- ((myData7$b*((myData7$y)/(1+myData7$z)))-(myData7$c*myData7$k))
-
-myData7$lambdapos[myData7$lambda>0] <- 1
-myData7$lambdapos[myData7$lambda<=0] <- 0
-
-myData7$deltapos[myData7$delta>0] <- 1
-myData7$deltapos[myData7$delta<=0] <- 0
-summary(myData7$lambdapos)
-summary(myData7$deltapos)
-# These summaries show us a roughly 15% reduction in positive utility possibilities
-
-require(ggplot2)
-lambda.myData7 <- qplot(myData7$lambda, geom="density") + 
-  xlab("Negative Campaigning Utility") + 
-  ylab("Density for RCV Elections - 7 Candidate Race") +
-  theme_bw()
-
-lambda.myData7
-
-require(ggplot2)
-delta.myData7 <- qplot(myData7$delta, geom="density") + 
-  xlab("Negative Campaigning Utility") + 
-  ylab("Density for non-RCV Elections - 7 Candidate Race") +
-  theme_bw()
-
-delta.myData7
-
-library(doBy)
-lambdaavgs.myData7 <- summaryBy(lambda ~ d, data=myData7, FUN=c(mean), na.rm=T)
-
-########################
-# 8 Candidate Election #
-########################
-
-library(foreign)
-myData8 <- read.csv("simdata8cands.csv")
-myData8$lambda <- ((myData8$b*(myData8$y+myData8$d)/(1+myData8$z))-(myData8$c*myData8$k))
-myData8$delta <- ((myData8$b*((myData8$y)/(1+myData8$z)))-(myData8$c*myData8$k))
-
-myData8$lambdapos[myData8$lambda>0] <- 1
-myData8$lambdapos[myData8$lambda<=0] <- 0
-
-myData8$deltapos[myData8$delta>0] <- 1
-myData8$deltapos[myData8$delta<=0] <- 0
-summary(myData8$lambdapos)
-summary(myData8$deltapos)
-# These summaries show us a roughly 15% reduction in positive utility possibilities
-
-require(ggplot2)
-lambda.myData8 <- qplot(myData8$lambda, geom="density") + 
-  xlab("Negative Campaigning Utility") + 
-  ylab("Density for RCV Elections - 8 Candidate Race") +
-  theme_bw()
-
-lambda.myData8
-
-require(ggplot2)
-delta.myData8 <- qplot(myData8$delta, geom="density") + 
-  xlab("Negative Campaigning Utility") + 
-  ylab("Density for non-RCV Elections - 8 Candidate Race") +
-  theme_bw()
-
-delta.myData8
-
-library(doBy)
-lambdaavgs.myData8 <- summaryBy(lambda ~ d, data=myData8, FUN=c(mean), na.rm=T)
-
-
-########################
-# 9 Candidate Election #
-########################
-
-library(foreign)
-myData9 <- read.csv("simdata9cands.csv")
-myData9$lambda <- ((myData9$b*(myData9$y+myData9$d)/(1+myData9$z))-(myData9$c*myData9$k))
-myData9$delta <- ((myData9$b*((myData9$y)/(1+myData9$z)))-(myData9$c*myData9$k))
-
-myData9$lambdapos[myData9$lambda>0] <- 1
-myData9$lambdapos[myData9$lambda<=0] <- 0
-
-myData9$deltapos[myData9$delta>0] <- 1
-myData9$deltapos[myData9$delta<=0] <- 0
-summary(myData9$lambdapos)
-summary(myData9$deltapos)
-# These summaries show us a roughly 15% reduction in positive utility possibilities
-
-require(ggplot2)
-lambda.myData9 <- qplot(myData9$lambda, geom="density") + 
-  xlab("Negative Campaigning Utility") + 
-  ylab("Density for RCV Elections - 9 Candidate Race") +
-  theme_bw()
-
-lambda.myData9
-
-require(ggplot2)
-delta.myData9 <- qplot(myData9$delta, geom="density") + 
-  xlab("Negative Campaigning Utility") + 
-  ylab("Density for non-RCV Elections - 9 Candidate Race") +
-  theme_bw()
-
-delta.myData9
-
-library(doBy)
-lambdaavgs.myData9 <- summaryBy(lambda ~ d, data=myData9, FUN=c(mean), na.rm=T)
-
 #############################################
 # Rerun all functions without b and c terms #
 #############################################
+
+########################
+# 2 Candidate Election #
+########################
+library(foreign)
+myData2 <- read.csv("simdata2cands.csv")
+myData2$lambda.n <- (((myData2$y+myData2$d)/(1+myData2$z))-(myData2$k))
+myData2$delta.n <- ((((myData2$y)/(1+myData2$z)))-(myData2$k))
+
+myData2$lambdapos.n[myData2$lambda.n>0] <- 1
+myData2$lambdapos.n[myData2$lambda.n<=0] <- 0
+
+myData2$deltapos.n[myData2$delta.n>0] <- 1
+myData2$deltapos.n[myData2$delta.n<=0] <- 0
+summary(myData2$lambdapos.n)
+summary(myData2$deltapos.n)
+# These summaries show a 3% reduction in positive utility possibilities
+
+require(ggplot2)
+lambda.myData2.n <- qplot(myData2$lambda.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for RCV Elections - 2 Candidate Race") +
+  theme_bw()
+
+lambda.myData2.n
+
+require(ggplot2)
+delta.myData2.n <- qplot(myData2$delta.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for non-RCV Elections - 2 Candidate Race") +
+  theme_bw()
+
+delta.myData2.n
+
+library(doBy)
+lambdaavgs.myData2.n <- summaryBy(lambda.n ~ d, data=myData2, FUN=c(mean), na.rm=T)
+
+
+########################
+# 3 Candidate Election #
+########################
+library(foreign)
+myData3 <- read.csv("simdata3cands.csv")
+myData3$lambda.n <- (((myData3$y+myData3$d)/(1+myData3$z))-(myData3$k))
+myData3$delta.n <- ((((myData3$y)/(1+myData3$z)))-(myData3$k))
+
+myData3$lambdapos.n[myData3$lambda.n>0] <- 1
+myData3$lambdapos.n[myData3$lambda.n<=0] <- 0
+
+myData3$deltapos.n[myData3$delta.n>0] <- 1
+myData3$deltapos.n[myData3$delta.n<=0] <- 0
+summary(myData3$lambdapos.n)
+summary(myData3$deltapos.n)
+# These summaries show a 3% reduction in positive utility possibilities
+
+require(ggplot2)
+lambda.myData3.n <- qplot(myData3$lambda.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for RCV Elections - 3 Candidate Race") +
+  theme_bw()
+
+lambda.myData3.n
+
+require(ggplot2)
+delta.myData3.n <- qplot(myData3$delta.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for non-RCV Elections - 3 Candidate Race") +
+  theme_bw()
+
+delta.myData3.n
+
+library(doBy)
+lambdaavgs.myData3.n <- summaryBy(lambda.n ~ d, data=myData3, FUN=c(mean), na.rm=T)
+
+
+########################
+# 4 Candidate Election #
+########################
+library(foreign)
+myData4 <- read.csv("simdata4cands.csv")
+myData4$lambda.n <- (((myData4$y+myData4$d)/(1+myData4$z))-(myData4$k))
+myData4$delta.n <- ((((myData4$y)/(1+myData4$z)))-(myData4$k))
+
+myData4$lambdapos.n[myData4$lambda.n>0] <- 1
+myData4$lambdapos.n[myData4$lambda.n<=0] <- 0
+
+myData4$deltapos.n[myData4$delta.n>0] <- 1
+myData4$deltapos.n[myData4$delta.n<=0] <- 0
+summary(myData4$lambdapos.n)
+summary(myData4$deltapos.n)
+# These summaries show a 4% reduction in positive utility possibilities
+
+require(ggplot2)
+lambda.myData4.n <- qplot(myData4$lambda.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for RCV Election - 4 Candidate Race") +
+  theme_bw()
+
+lambda.myData4.n
+
+require(ggplot2)
+delta.myData4.n <- qplot(myData4$delta.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for non-RCV Elections - 4 Candidate Race") +
+  theme_bw()
+
+delta.myData4.n
+
+library(doBy)
+lambdaavgs.myData4.n <- summaryBy(lambda.n ~ d, data=myData4, FUN=c(mean), na.rm=T)
+
+########################
+# 5 Candidate Election #
+########################
+library(foreign)
+myData5 <- read.csv("simdata5cands.csv")
+myData5$lambda.n <- (((myData5$y+myData5$d)/(1+myData5$z))-(myData5$k))
+myData5$delta.n <- ((((myData5$y)/(1+myData5$z)))-(myData5$k))
+
+myData5$lambdapos.n[myData5$lambda.n>0] <- 1
+myData5$lambdapos.n[myData5$lambda.n<=0] <- 0
+
+myData5$deltapos.n[myData5$delta.n>0] <- 1
+myData5$deltapos.n[myData5$delta.n<=0] <- 0
+summary(myData5$lambdapos.n)
+summary(myData5$deltapos.n)
+# These summaries show a 5.4% reduction in positive utility possibilities
+
+require(ggplot2)
+lambda.myData5.n <- qplot(myData5$lambda.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for RCV Elections - 5 Candidate Race") +
+  theme_bw()
+
+lambda.myData5.n
+
+require(ggplot2)
+delta.myData5.n <- qplot(myData5$delta.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for non-RCV Elections - 5 Candidate Race") +
+  theme_bw()
+
+delta.myData5.n
+
+library(doBy)
+lambdaavgs.myData5.n <- summaryBy(lambda.n ~ d, data=myData5, FUN=c(mean), na.rm=T)
+
+########################
+# 6 Candidate Election #
+########################
+
+library(foreign)
+myData6 <- read.csv("simdata6cands.csv")
+myData6$lambda.n <- (((myData6$y+myData6$d)/(1+myData6$z))-(myData6$k))
+myData6$delta.n <- ((((myData6$y)/(1+myData6$z)))-(myData6$k))
+
+myData6$lambdapos.n[myData6$lambda.n>0] <- 1
+myData6$lambdapos.n[myData6$lambda.n<=0] <- 0
+
+myData6$deltapos.n[myData6$delta.n>0] <- 1
+myData6$deltapos.n[myData6$delta.n<=0] <- 0
+summary(myData6$lambdapos.n)
+summary(myData6$deltapos.n)
+# These summaries show us a roughly 15% reduction in positive utility possibilities
+
+require(ggplot2)
+lambda.myData6.n <- qplot(myData6$lambda.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for RCV Elections - 6 Candidate Race") +
+  theme_bw()
+
+lambda.myData6.n
+
+require(ggplot2)
+delta.myData6.n <- qplot(myData6$delta.n, geom="density") + 
+  xlab("Negative Campaigning Utility") + 
+  ylab("Density for non-RCV Elections - 6 Candidate Race") +
+  theme_bw()
+
+delta.myData6.n
+
+library(doBy)
+lambdaavgs.myData6.n <- summaryBy(lambda.n ~ d, data=myData6, FUN=c(mean), na.rm=T)
 
 
 ##################
 # Candidate Data #
 ##################
 library(foreign)
-canddata <- read.csv("candidatercv.csv")
+canddat <- read.csv("candidatercv.csv")
+names(canddat)[65] <- "spent"
+
+canddat$neg[canddat$B8rival==6] <- 0
+canddat$neg[canddat$B8rival==1] <- 0
+canddat$neg[canddat$B8rival==2] <- 0
+canddat$neg[canddat$B8rival==3] <- 1
+canddat$neg[canddat$B8rival==4] <- 2
+canddat$neg[canddat$B8rival==5] <- 2
+
+canddat$neg.0[canddat$neg==0] <- 1
+canddat$neg.0[canddat$neg!=0] <- 0
+
+canddat$neg.1[canddat$neg==1] <- 1
+canddat$neg.1[canddat$neg!=1] <- 0
+
+canddat$neg.2[canddat$neg==2] <- 1
+canddat$neg.2[canddat$neg!=2] <- 0
+
+canddat$neg.f <- factor(canddat$neg, labels=c("Mostly/Somewhat Positive", "Neither Positive/Negative", "Somewhat/Mostly Negative"))
+canddat$candvoteshare <- as.numeric(canddat$candvoteshare)
+canddat$age <- as.numeric(canddat$age)
+canddat$B1interest <- as.numeric(canddat$B1interest)
+canddat$C3educ <- as.numeric(canddat$C3educ)
+canddat$C6female <- as.numeric(canddat$C6female)
+canddat$spent <- as.numeric(canddat$spent)
+
+canddat$neglogit[canddat$B8rival==6] <- 0
+canddat$neglogit[canddat$B8rival==1] <- 0
+canddat$neglogit[canddat$B8rival==2] <- 0
+canddat$neglogit[canddat$B8rival==3] <- 0
+canddat$neglogit[canddat$B8rival==4] <- 1
+canddat$neglogit[canddat$B8rival==5] <- 1
+
+canddat$info[canddat$B3info==1] <- 4
+canddat$info[canddat$B3info==2] <- 3
+canddat$info[canddat$B3info==3] <- 2
+canddat$info[canddat$B3info==4] <- 1
+
+canddat$rivalneg[canddat$B7you==6] <- 0
+canddat$rivalneg[canddat$B7you==5] <- 1
+canddat$rivalneg[canddat$B7you==4] <- 1
+canddat$rivalneg[canddat$B7you==3] <- 0
+canddat$rivalneg[canddat$B7you==2] <- 0
+canddat$rivalneg[canddat$B7you==1] <- 0
+
+canddat$emp[canddat$C1emp==1] <- 1
+canddat$emp[canddat$C1emp==2] <- 1
+canddat$emp[canddat$C1emp==3] <- 0
+canddat$emp[canddat$C1emp==4] <- 0
+canddat$emp[canddat$C1emp==5] <- 0
+canddat$emp[canddat$C1emp==6] <- 0
+canddat$emp[canddat$C1emp==7] <- 0
+
+canddat$white[canddat$C4race==1] <- 1
+canddat$white[canddat$C4race==0] <- 0
+canddat$white[canddat$C4race==2] <- 0
+canddat$white[canddat$C4race==3] <- 0
+canddat$white[canddat$C4race==4] <- 0
+canddat$white[canddat$C4race==5] <- 0
+canddat$white[canddat$C4race==6] <- 0
+
+# remove missing cases
+# remove missing values of the weight variable
+myDat0 <- canddat[complete.cases(canddat[, c("B8rival")]), ]
+myDat1 <- myDat0[complete.cases(myDat0[, c("spent")]), ]
+myDat2 <- myDat1[complete.cases(myDat1[, c("info")]), ]
+myDat3 <- myDat2[complete.cases(myDat2[, c("B1interest")]), ]
+myDat4 <- myDat3[complete.cases(myDat3[, c("rivalneg")]), ]
+myDat5 <- myDat4[complete.cases(myDat4[, c("recentwin")]), ]
+myDat6 <- myDat5[complete.cases(myDat5[, c("candvoteshare")]), ]
+myDat7 <- myDat6[complete.cases(myDat6[, c("emp")]), ]
+myDat8 <- myDat7[complete.cases(myDat7[, c("C3educ")]), ]
+myDat9 <- myDat8[complete.cases(myDat8[, c("white")]), ]
+myDat10 <- myDat9[complete.cases(myDat9[, c("C6female")]), ]
+canddata <- myDat10[complete.cases(myDat10[, c("age")]), ]
+canddata <- canddata[canddata$B8rival!=6, ]
+
+canddata$spent <- log(canddata$spent)
+
+require(MASS)
+olog.neg <- polr(neg.f ~ rcv + recentwin + rivalneg + candvoteshare + spent, data=canddata, Hess = TRUE)
+olog.neg2 <- polr(neg.f ~ rcv + recentwin + candvoteshare + spent, data=canddata, Hess = TRUE)
+
+
+require(stargazer)
+stargazer(olog.neg, olog.neg2,
+          title=c("Effect of Electoral System Type on Decision to go Negative"),
+          dep.var.labels=c("Negativity"),
+          covariate.labels=c("RCV",
+                             "Recent Winner",
+                             "Rival Negativity",
+                             "Vote Share",
+                             "Spending(log)"),
+          notes=c("Unstandardized ordered logistic coefficients with standard errors below in parantheses.  Source: Candidate Survey")
+          )
+
+
